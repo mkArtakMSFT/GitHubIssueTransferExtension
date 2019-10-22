4c9e2367979d36b1a15bb23758d7d1d9c4b7ea39
Artak <artak@outlook.com>
2019-10-22 11:27:23 -0700

    Fixed the input finding logic.

--- src/extension/js/contentScript.js
+++ src/extension/js/contentScript.js
@@ -3,20 +3,12 @@ function transfer() {
     let url = document.location.href.toLowerCase();
 
     if (url.indexOf("/issues") > 0) {
-        let checkAll = $("#js-issues-toolbar > div.mr-3 > input[type=checkbox]");
-        // checkAll.prop('checked', true);
-        let issueRows = $("div[id^='issue_']");
-        // alert('Selected count: ' + issueRows.length);
+        let issueRows = $("div[aria-label='Issues']>div input:checkbox:checked");
+        console.log('Selected count: ' + issueRows.length);
 
         issueRows.each(function () {
-            let selectionCheckbox = $(this).children("input[type='checkbox']");
-            console.log(selectionCheckbox);
-            console.log(selectionCheckbox.attr('name'));
-            // console.log(selectionCheckbox.attr('id'));
-            if (selectionCheckbox.prop('checked'))
-            {
-                alert ("checked");
-            }
+            let selectedCheckboxes = $(this);
+            console.log(selectedCheckboxes.attr('value'));
         });
     }
 }
