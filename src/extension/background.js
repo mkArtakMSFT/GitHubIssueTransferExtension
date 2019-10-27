399b2fb57830c658efcb8797121136227e228383
Artak <artak@outlook.com>
2019-10-26 22:57:16 -0700

    Removed legacy comments

--- src/extension/background.js
+++ src/extension/background.js
@@ -1,7 +1,3 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function () {
@@ -10,19 +6,14 @@ chrome.runtime.onInstalled.addListener(function () {
   });
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
-    // chrome.storage.local.get(['owner', 'transferFrom', 'transferTo'], function (result) {
-    //   let newHost = 'github.com/' + result.owner + '/' + result.transferFrom + '/issues';
-    //   newHost = newHost.toLowerCase();
-      
-      let newHost = 'github.com';
-      console.log("Adding rule for new host: " + newHost);
+    let newHost = 'github.com';
+    console.log("Adding rule for new host: " + newHost);
 
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostEquals: newHost },
-        })],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    //});
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: newHost },
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
   });
 });
